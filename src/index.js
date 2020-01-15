a89e83087cf2904654ce322462a272c476bfdd93
Ji Hua <michaelhua1993@foxmail.com>
2020-01-15 00:31:36 -0500

    add table

--- src/index.js
+++ src/index.js
@@ -1,73 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import templates from './game_templates.json'
+import templates_data from './game_templates.json'
 import './index.css';
-import { realpathSync } from 'fs';
+import { Button, Table} from 'react-bootstrap';
 
 // var headers = [
 //   "号码", "姓名", "角色", "技能", "死亡情况", "警徽"
 // ];
 
-// var players = [
-//   ["1", "Player1", "", "", "", ""],
-//   ["2", "Player2", "", "", "", ""],
-//   ["3", "Player3", "", "", "", ""],
-//   ["4", "Player4", "", "", "", ""],
-//   ["5", "Player5", "", "", "", ""],
-//   ["6", "Player6", "", "", "", ""],
-//   ["7", "Player7", "", "", "", ""],
-//   ["8", "Player8", "", "", "", ""],
-//   ["9", "Player9", "", "", "", ""],
-//   ["10", "Player10", "", "", "", ""],
-//   ["11", "Player11", "", "", "", ""],
-//   ["12", "Player12", "", "", "", ""]
-// ];
 
 class Infobox extends React.Component {
   render() {
     return (
-      <p> 该游戏包含以下角色：<br></br>
+      <div> <p>该游戏包含以下角色：</p>
         {this.props.value.map(pair => {
-          return (<ul><strong>{pair[0]}</strong> {pair[1]}名</ul>)
+          return (<strong> {pair[1]}名{pair[0]} </strong>)
         })}
-      </p>
+      </div>
     )
   }
 }
 
 
 
-class Template extends React.Component {
+class GameSelectBox extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      "data": templates,
-      "value": "default0",
-    };
+      "value": "default0"
+    }
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  renderInfobox(value) {
-    var characters = this.state.data.find(function(e) {
-      return e.name === value;
-    }).characters.map(c => {
-      return [c.display_name, c.number];
-    });
-    return <Infobox value={characters} />
-  }
 
   handleChange(event) {
     this.setState({value: event.target.value});
-    this.renderInfobox(event.target.value);
   }
 
-  handleSubmit(event) {
-    
+  handleSubmit() {
+    this.props.action(this.state.value);
   }
 
   render() {
+    const selectedValue = this.state.value;
+    var characters = this.props.data.find(function(e) {
+      return e.name === selectedValue;
+    }).characters.map(c => {
+      return [c.display_name, c.number];
+    });
+
     return (
       <form>
         <label>
@@ -75,72 +58,117 @@ class Template extends React.Component {
           <br></br>
           <select value={this.state.value} onChange={this.handleChange}>
             {
-              this.state.data.map(element => {
+              this.props.data.map(element => {
                 return <option value={element.name}>{element.display_name}</option>;
               })
             }
           </select>
+          <Button onClick={this.handleSubmit} variant="primary">确认</Button>
         </label>
         <br />
-        {this.renderInfobox(this.state.value)}
-        <input type="submit" value="Submit" />
+        <Infobox value={characters}/>
+        
       </form>
     );
   }
 }
 
+class PlayerRow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      "character": "",
+      "death": "存活",
+      "badge": false
+    };
+  }
 
-class Square extends React.Component {
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
+      <tr>
+  <td id={"player-" + this.props.playerId + "-id"}>{this.props.playerId}</td>
+        <td><input type="text" value={this.props.playerId + "号玩家"}></input></td>
+        <td>{this.state.character}</td>
+        <td>{}</td>
+        <td>{}</td>
+        <td>{this.state.death}</td>
+        <td>{this.state.badge ? "*": ""}</td>
+      </tr>
+    )
+    
   }
 }
 
+
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
+  constructor(props) {
+    super(props);
+    this.state = {};
+
   }
 
   render() {
-    const status = 'Next player: X';
-
+    const nPlayer = this.props.nplayers;
+    var playerIds = Array.from(Array(nPlayer).keys()).map(k => {return k + 1});
+    console.log(playerIds)
     return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+      <Table striped bordered hover responsive="md">
+        <thead>
+          <tr>
+            <th>号码</th>
+            <th>姓名</th>
+            <th>角色</th>
+            <th>阵营</th>
+            <th>技能</th>
+            <th>存活情况</th>
+            <th>警徽</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            playerIds.map(pid => {
+              return <PlayerRow playerId={pid}/>;
+            })   
+          }
+          
+        </tbody>
+      </Table>
+    )
   }
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      "nplayers":  12
+    };
+
+    this.handleGameSelect = this.handleGameSelect.bind(this);
+  }
+
+  handleGameSelect(value) {
+    var data = this.props.templates.find(e => {
+      return e.name === value
+    })
+    this.setState({template: data});
+    this.setState({nplayers: data.total_players})
+
+  }
+
   render() {
+    var templatesData = this.props.templates;
     return (
       <div className="game">
+        <h1>狼人杀裁判小助手</h1>
+        <br></br>
+        <div className="game-select-box">
+          <GameSelectBox data={templatesData} action={this.handleGameSelect} />
+        </div>
         <div className="game-board">
-          <Board />
+          <Board nplayers={this.state.nplayers} />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
         </div>
       </div>
     );
@@ -150,6 +178,6 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(
-  <Template />,
+  <Game templates={templates_data}/>,
   document.getElementById('root')
 );
